58e81501a6a44775d486fc8788d7f4da1e2fc33c
Grzegorz Ilnicki <Grzegorz.Ilnicki@colonnade.pl>
2025-11-25 23:05:04 +0100

    chore: correct local dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -37,8 +37,8 @@ module.exports = {
 	},
 	devServer: {
 		host: '0.0.0.0',
-		port: 9000,
-		disableHostCheck: true
+		port: 9000
+		// disableHostCheck: true
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
